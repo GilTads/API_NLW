13a85c3f756a92998b1cdad95e5e044ebe48ebdf
GilTads <gil.tads@gmail.com>
2020-06-12 11:51:28 -0400

    inserting '/' route for tests

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,12 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 import PointsController from './controller/PointsController'
 import ItemsController from './controller/ItemsController'
 
 const routes = express.Router()
 const pointsController = new PointsController()
 const itemsController = new ItemsController()
+
+routes.get('/', (req: Request, res: Response) => res.json('API NLW running...'))
 
 routes.get('/items', itemsController.index)
 
