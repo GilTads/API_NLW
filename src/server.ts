cf41e9ec1aa444ecdf2d92ef653ad7cbb16dee50
gilTads <gil.tads@gmail.com>
2020-06-15 23:08:50 -0400

    add server message

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,13 @@ import cors from 'cors'
 import path from 'path'
 import routes from './routes'
 
+const port = 3000
+
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(routes)
 app.use('/assets/images', express.static(path.resolve(__dirname, '..', 'assets/images')))
-app.listen(3000)
+app.listen(port, () => console.info(`Up and running in ${port} port`))
 
